0a6a67b26cc0f3888930874fb8557479cc9b3c60
Simon Zorin <simon.zorin13@gmail.com>
2023-08-01 16:12:16 -0400

    restructure repo

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
- backend "s3" {
-   bucket = var.aws_backend_bucketname
-   key = var.aws_backend_bucket_key
-   region = var.aws_region
-   dynamodb_table = var.aws_dynamodb_table
- }
-}
