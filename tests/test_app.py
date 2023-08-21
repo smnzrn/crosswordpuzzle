25fe57ba16dd582af3e42d6198e35df7abd9bceb
Simon Zorin <simon.zorin13@gmail.com>
2023-08-21 19:25:25 -0400

    modify

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,20 +1,21 @@
-from app import app
 import unittest
+from app.app import app  # Assuming your Flask app instance is named `app`
 
 class FlaskTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.app = app.app.test_client()
+        self.app = app.test_client()
+        self.app.testing = True
 
-    def test_index(self):
-        rv = self.app.get('/')
-        self.assertEqual(rv.status_code, 200)
-        self.assertIn(b'This is the crossword puzzle.', rv.data)
+    def test_index_page(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        # Add any additional assertions you might need. For instance, checking if certain words or titles are in the response
 
-    def test_about(self):
-        rv = self.app.get('/about')
-        self.assertEqual(rv.status_code, 200)
-        self.assertIn(b'This is the about page.', rv.data)
+    def test_about_page(self):
+        response = self.app.get('/about')
+        self.assertEqual(response.status_code, 200)
+        # Again, add any additional assertions that are necessary
 
 if __name__ == '__main__':
     unittest.main()
