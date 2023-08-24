87be9bcec4593d603f42613839fac6c042c57646
Simon Zorin <simon.zorin13@gmail.com>
2023-08-24 17:56:42 -0400

    fix github actions file

--- bootstrap/staticfiles-bucket.tf
+++ bootstrap/staticfiles-bucket.tf
@@ -11,3 +11,8 @@ resource "aws_s3_bucket" "staticfiles-bucket" {
     object_lock_enabled = "Enabled"
   }
 }
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.statemgmt-bucket.bucket
+  description = "The name of the S3 bucket used for Terraform state."
+}
