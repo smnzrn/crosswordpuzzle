144a12b961c388e6d4ef5816a98f51f27b24eff0
Simon Zorin <simon.zorin13@gmail.com>
2023-08-23 21:36:40 -0400

    fix ecr issues

--- main.tf
+++ main.tf
@@ -1,35 +1,3 @@
-resource "aws_ecr_repository" "my_repository" {
-  name                 = "crosswordpuzzle-repository"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
-
-resource "aws_ecr_lifecycle_policy" "delete_old_images" {
-  repository = aws_ecr_repository.my_repository.name
-
-  policy = <<EOF
-{
-    "rules": [
-        {
-            "rulePriority": 1,
-            "description": "Keep last 10 images",
-            "selection": {
-                "tagStatus": "untagged",
-                "countType": "imageCountMoreThan",
-                "countNumber": 10
-            },
-            "action": {
-                "type": "expire"
-            }
-        }
-    ]
-}
-EOF
-}
-
 resource "aws_ecs_cluster" "crossword_cluster" {
   name = "crossword-cluster"
 }
