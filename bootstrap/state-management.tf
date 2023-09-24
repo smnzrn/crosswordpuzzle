51f6f06beaf768b6182d81d60e6249893a75002c
Simon Zorin <simon.zorin13@gmail.com>
2023-09-23 23:14:12 -0400

    fix att error

--- bootstrap/state-management.tf
+++ bootstrap/state-management.tf
@@ -1,26 +1,19 @@
+variable "check_existing_table" {
+  description = "Set to true if you want to check for the existing table. Default is false, meaning it will always try to create a new table."
+  default     = false
+}
+
 data "aws_dynamodb_table" "existing" {
-  name = "terraform_state"
+  count = var.check_existing_table ? 1 : 0
+  name  = "terraform_state"
 }
 
-resource "random_string" "bucket_suffix_stmgmt" {
-  length  = 8
-  upper   = false
-  number  = true
-  special = false
-}
-
-resource "aws_s3_bucket" "statemgmt-bucket" {
-  bucket = "${var.statemgmt_bucket_name}-${random_string.bucket_suffix_stmgmt.result}"
-  object_lock_configuration {
-    object_lock_enabled = "Enabled"
-  }
-}
-  
 resource "aws_dynamodb_table" "terraform-lock" {
-  count = data.aws_dynamodb_table.existing.exists ? 0 : 1
-  name = "terraform_state"
+  count = var.check_existing_table && length(data.aws_dynamodb_table.existing.*.name) > 0 ? 0 : 1
+
+  name         = "terraform_state"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key = "LockID"
+  hash_key     = "LockID"
 
   attribute {
     name = "LockID"
